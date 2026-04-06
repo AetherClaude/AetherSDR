d0c64a4631e4877d5679ffc4cb4cf7dc17c33134
CJ Johnson <chibondking@users.noreply.github.com>
2026-04-06 09:55:08 -0500

    Fix/issue 53 slice pan limit notifications (#852)
    * test: verify GPG signing
    
    * Signing with key
    
    * add in a check that includes ALL clients (i.e. MultiFlex)

--- src/models/RadioModel.cpp
+++ src/models/RadioModel.cpp
@@ -491,12 +491,14 @@ void RadioModel::addSlice()
     const QString cmd = QString("slice create pan=%1 freq=%2").arg(m_activePanId, freq);
 
     qCDebug(lcProtocol) << "RadioModel::addSlice:" << cmd;
-    sendCmd(cmd, [](int code, const QString& body) {
-        if (code != 0)
+    sendCmd(cmd, [this](int code, const QString& body) {
+        if (code != 0) {
             qCWarning(lcProtocol) << "RadioModel: slice create failed, code"
                        << Qt::hex << code << "body:" << body;
-        else
+            emit sliceCreateFailed(maxSlices(), m_model);
+        } else {
             qCDebug(lcProtocol) << "RadioModel: new slice created, index =" << body;
+        }
     });
 }
 
@@ -517,17 +519,26 @@ void RadioModel::addSliceOnPan(const QString& panId)
     const QString cmd = QString("slice create pan=%1 freq=%2").arg(panId, freq);
 
     qCDebug(lcProtocol) << "RadioModel::addSliceOnPan:" << cmd;
-    sendCmd(cmd, [](int code, const QString& body) {
-        if (code != 0)
+    sendCmd(cmd, [this](int code, const QString& body) {
+        if (code != 0) {
             qCWarning(lcProtocol) << "RadioModel: slice create failed, code"
                        << Qt::hex << code << "body:" << body;
-        else
+            emit sliceCreateFailed(maxSlices(), m_model);
+        } else {
             qCDebug(lcProtocol) << "RadioModel: new slice created, index =" << body;
+        }
     });
 }
 
 void RadioModel::createPanadapter()
 {
+    int limit = maxPanadapters();
+    if (static_cast<int>(m_panadapters.size()) >= limit) {
+        qCWarning(lcProtocol) << "RadioModel::createPanadapter: limit of" << limit
+                              << "panadapters reached for model" << m_model;
+        emit panadapterLimitReached(limit, m_model);
+        return;
+    }
     qCDebug(lcProtocol) << "RadioModel::createPanadapter: sending display panafall create";
     sendCmd("display panafall create x=100 y=100", [this](int code, const QString& body) {
         if (code != 0) {
