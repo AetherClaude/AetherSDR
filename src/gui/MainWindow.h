4d56760d2d3c5995497f213ab78c8418f8490654
jensenpat <84298137+jensenpat@users.noreply.github.com>
2026-04-10 21:04:21 -0700

    Check macOS DAX driver before enabling (#1141)

--- src/gui/MainWindow.h
+++ src/gui/MainWindow.h
@@ -303,7 +303,7 @@ private:
 #if defined(Q_OS_MAC) || defined(HAVE_PIPEWIRE)
     DaxBridge* m_daxBridge{nullptr};
     QString m_savedMicSelection;  // restore on stopDax
-    void startDax();
+    bool startDax();
     void stopDax();
 #endif
 };
